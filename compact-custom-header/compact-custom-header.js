2dcaec0648a6c0d55e9c11fc62f472af1f227a7f
MKR <25127328+maykar@users.noreply.github.com>
2018-12-09 00:55:10 -0500

    Update compact-custom-header.js

--- compact-custom-header/compact-custom-header.js
+++ compact-custom-header/compact-custom-header.js
@@ -1,6 +1,6 @@
 class CompactCustomHeader extends HTMLElement {
   set hass(hass) {
-    window.cch_header = this.config.header == undefined ? true : this.config.header
+    window.cch_header = this.config.header == undefined ? true : this.config.header;
     window.cch_menu = this.config.menu == undefined ? true : this.config.menu;
     window.cch_notify = this.config.notification == undefined ? true : this.config.notification;
     window.cch_voice = this.config.voice == undefined ? true : this.config.voice;
@@ -8,7 +8,6 @@ class CompactCustomHeader extends HTMLElement {
     window.cch_clock = this.config.clock == undefined ? true : this.config.clock;
     window.cch_clock_format = this.config.clock_format == undefined ? 12 : this.config.clock_format;
     window.cch_am_pm = this.config.clock_am_pm == undefined ? true : this.config.clock_am_pm;
-
     const script = document.createElement('script');
     script.src = '/local/custom-lovelace/compact-custom-header/cch_v001.js';
     document.head.appendChild(script);
@@ -20,5 +19,4 @@ class CompactCustomHeader extends HTMLElement {
     return 0;
   }
 }
-
 customElements.define('compact-custom-header', CompactCustomHeader);
